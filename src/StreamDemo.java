c71b673a6c8007da5a897c0d1c37b303a590040b
wangj <icewind4096@gmail.com>
2018-03-01 22:24:58 +0800

    在stream中添加了字符流操作

--- src/StreamDemo.java
+++ src/StreamDemo.java
@@ -5,17 +5,41 @@ import java.io.*;
  */
 public class StreamDemo {
     public static void main(String[] args) throws IOException {
-        streamOverWriteTest();
-        streamAppendTest();
-        charTest();
-    }
-
-    private static void charTest() {
-    }
-
-    private static void streamAppendTest() throws IOException {
         File file = new File("d:" + File.separatorChar + "level" + File.separatorChar + "streamOutput.txt");
 
+        writerTest(file);
+
+//        streamOverWriteTest(file);
+//        streamAppendTest(file);
+        readTest(file);
+    }
+
+    private static void writerTest(File file) throws IOException {
+        if (file.getParentFile().exists() == false){
+            file.getParentFile().mkdirs();
+        }
+
+        Writer writer = new FileWriter(file);
+        writer.write("ABC");
+        writer.append("中国红");
+        writer.close();
+    }
+
+    private static void readTest(File file) throws IOException {
+        if (file.exists() == true) {
+            InputStream inputStream = new FileInputStream(file);
+            if (file.length() > 0) {
+                int length = (int) file.length();
+                byte[] bytes = new byte[length - 1];
+                inputStream.read(bytes);
+                String text = new String(bytes);
+                System.out.println(text);
+            }
+            inputStream.close();
+        }
+    }
+
+    private static void streamAppendTest(File file) throws IOException {
         //判断目录是否存在,必须
         if (file.getParentFile().exists() == false){
             file.getParentFile().mkdirs();
@@ -27,9 +51,7 @@ public class StreamDemo {
         outputStream.close();
     }
 
-    private static void streamOverWriteTest() throws IOException {
-        File file = new File("d:" + File.separatorChar + "level" + File.separatorChar + "streamOutput.txt");
-
+    private static void streamOverWriteTest(File file) throws IOException {
         //判断目录是否存在,必须
         if (file.getParentFile().exists() == false){
             file.getParentFile().mkdirs();
